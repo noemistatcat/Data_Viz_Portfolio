48f2aa71067a184e1cfb1b3800cb3c0edca36a62
Noemi Ramiro <noemi.ramiro18@gmail.com>
2021-04-26 09:30:59 +0800

    Final version

--- online_retail_cohort_analysis.sql
+++ online_retail_cohort_analysis.sql
@@ -1,23 +1,7 @@
- DROP TABLE IF EXISTS `practice-datasets-310713.online_retail_datasets.online_retail_cohort_analysis`;
- CREATE TABLE `practice-datasets-310713.online_retail_datasets.online_retail_cohort_analysis`
- AS 
-    WITH t_first_purchase AS (
-    SELECT DISTINCT
-    FORMAT_DATETIME('%Y%m', first_purchase_date) AS first_purchase,
-    DATE_DIFF(date, first_purchase_date, MONTH) AS month_order,
-    SUM(Sales) AS Sales,
-    COUNT(DISTINCT CustomerID) AS Customers
-    FROM (
-      SELECT 
-      DATE(TIMESTAMP(InvoiceDate)) AS date,
-      UnitPrice * Quantity AS Sales,
-      CustomerID,
-      FIRST_VALUE(DATE(TIMESTAMP(InvoiceDate))) OVER (PARTITION BY CustomerID ORDER BY DATE(TIMESTAMP(InvoiceDate))) AS first_purchase_date
-      FROM `practice-datasets-310713.online_retail_datasets.online_retail`
-      WHERE Quantity IS NOT NULL DROP TABLE IF EXISTS `practice-datasets-310713.online_retail_datasets.online_retail_cohort_analysis`;
- CREATE TABLE `practice-datasets-310713.online_retail_datasets.online_retail_cohort_analysis`
- AS 
-    WITH t_first_purchase AS (
+DROP TABLE IF EXISTS `practice-datasets-310713.online_retail_datasets.online_retail_cohort_analysis`;
+CREATE TABLE `practice-datasets-310713.online_retail_datasets.online_retail_cohort_analysis`
+AS    
+   WITH t_first_purchase AS (
     SELECT DISTINCT
     date,
     DATE_DIFF(date, first_purchase_date, MONTH) AS month_order,
@@ -34,7 +18,7 @@
       WHERE Quantity IS NOT NULL 
       AND UnitPrice IS NOT NULL
       AND CustomerID IS NOT NULL 
-      AND DATE(TIMESTAMP(InvoiceDate)) BETWEEN '2011-01-01' AND '2011-11-30'
+      AND DATE(TIMESTAMP(InvoiceDate)) BETWEEN '2010-12-01' AND '2011-11-30'
     )
    ),
 
@@ -42,7 +26,8 @@ t_agg AS (
     SELECT 
     first_purchase,
     month_order,
-    SUM(Sales) AS Sales,
+    SUM(Sales) AS TotalSales,
+    AVG(Sales) AS AvgSales,
     COUNT(DISTINCT CustomerID) AS Customers
     FROM 
     t_first_purchase
@@ -51,11 +36,12 @@ t_agg AS (
 
  t_cohort AS (
      SELECT *,
-     SAFE_DIVIDE(Sales, CohortSales) AS CohortSalesPerc,
-     SAFE_DIVIDE(Customers, CohortCustomers) AS CohortCustomersPerc  
+     SAFE_DIVIDE(TotalSales, CohortTotalSales) AS CohortSalesPerc,
+     SAFE_DIVIDE(Customers, CohortCustomers) AS CohortCustomersPerc
      FROM (
          SELECT *,
-         FIRST_VALUE(Sales) OVER (PARTITION BY first_purchase ORDER BY month_order) AS CohortSales,
+         FIRST_VALUE(TotalSales) OVER (PARTITION BY first_purchase ORDER BY month_order) AS CohortTotalSales,
+         FIRST_VALUE(AvgSales) OVER (PARTITION BY first_purchase ORDER BY month_order) AS CohortAvgSales,
          FIRST_VALUE(Customers) OVER (PARTITION BY first_purchase ORDER BY month_order) AS CohortCustomers
          FROM t_agg
      )
@@ -63,23 +49,4 @@ t_agg AS (
 
 SELECT * FROM t_cohort 
 ORDER BY first_purchase, month_order
-      AND UnitPrice IS NOT NULL
-      AND CustomerID IS NOT NULL 
-    )
-    GROUP BY first_purchase, month_order
-   ),
-
- t_cohort AS (
-     SELECT *,
-     SAFE_DIVIDE(Sales, CohortSales) AS CohortSalesPerc,
-     SAFE_DIVIDE(Customers, CohortCustomers) AS CohortCustomersPerc  
-     FROM (
-         SELECT *,
-         FIRST_VALUE(Sales) OVER (PARTITION BY first_purchase ORDER BY month_order) AS CohortSales,
-         FIRST_VALUE(Customers) OVER (PARTITION BY first_purchase ORDER BY month_order) AS CohortCustomers
-         FROM t_first_purchase
-     )
- )
-
-SELECT * FROM t_cohort 
-ORDER BY first_purchase, month_order
+ 
--- online_retail_preprocessed.sql
+++ online_retail_preprocessed.sql
@@ -2,16 +2,42 @@
  CREATE TABLE `practice-datasets-310713.online_retail_datasets.online_retail_preprocessed`
  PARTITION BY date
  AS 
- SELECT *,
- CASE WHEN customer_seq > 1 THEN 'Repeat Customer'
- ELSE 'New Customer'
- END AS RepeatPurchase 
- FROM ( 
-    SELECT  *,
-    DATE(TIMESTAMP(InvoiceDate)) AS date,
-    UnitPrice * Quantity AS Sales,
-    RANK() OVER (PARTITION BY CustomerID ORDER BY DATE(TIMESTAMP(InvoiceDate))) AS customer_seq
-    FROM `practice-datasets-310713.online_retail_datasets.online_retail`
-    WHERE Quantity IS NOT NULL 
-    AND UnitPrice IS NOT NULL
- )
+   WITH t_repeat AS (
+   SELECT *,
+   CASE WHEN customer_seq > 1 THEN 'Repeat Customer'
+   ELSE 'New Customer'
+   END AS RepeatPurchase 
+   FROM ( 
+      SELECT  *,
+      DATE(TIMESTAMP(InvoiceDate)) AS date,
+      UnitPrice * Quantity AS Sales,
+      RANK() OVER (PARTITION BY CustomerID ORDER BY DATE(TIMESTAMP(InvoiceDate))) AS customer_seq
+      FROM `practice-datasets-310713.online_retail_datasets.online_retail`
+      WHERE Quantity IS NOT NULL 
+      AND UnitPrice IS NOT NULL
+      AND CustomerID IS NOT NULL
+   )
+ ),
+
+ t_repurchase AS (
+    SELECT *, 
+    DATE_DIFF(date, first_purchase, MONTH) AS month_order
+    FROM (
+        SELECT *,
+        FIRST_VALUE(date) OVER (PARTITION BY CustomerID ORDER BY DATE) AS first_purchase
+        FROM t_repeat
+    )
+ ),
+
+  t_previous_purchase AS (
+    SELECT *, 
+    DATE_DIFF(next_purchase, date, DAY) AS days_bet_purchase
+    FROM (
+        SELECT *,
+        LEAD(date) OVER (PARTITION BY CustomerID ORDER BY DATE) AS next_purchase
+        FROM t_repurchase
+    )
+   )
+
+
+SELECT * FROM t_previous_purchase
